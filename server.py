8fafd62476abde9a8c189c08df8009fe6a9c9552
hellohanna <anylike90@gmail.com>
2019-07-20 15:38:26 -0700

    Sending a simple message

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from twilio.rest import Client
+from jinja2 import StrictUndefined
 
 # Your Account SID from twilio.com/console
 account_sid = os.environ["ACCOUNT_SID"]
@@ -10,20 +11,31 @@ auth_token  = os.environ["AUTH_TOKEN"]
 
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(
-    to="+15049136010", 
-    from_="+15592068349",
-    body="Hello from Python!")
 
-print(message.sid)
 
 
 app = Flask(__name__)
 db = SQLAlchemy()
 
 
+@app.route('/')
+def hello_world():
+    """Homepage."""
+
+    return render_template("add_affirmation.html")
 
 
+
+@app.route('/message', methods = ['GET','POST'])
+def send_message():
+    msg = request.form.get("user_affirmation", 0)
+    if msg:
+        message = client.messages.create(
+        to="+15049136010", 
+        from_="+15592068349",
+        body=msg)
+
+    return 'Message has been sent', 200
 
 def connect_to_db(app, database_uri="postgresql:///sampledb"):
     """Connect the database to Flask app."""
