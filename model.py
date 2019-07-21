360b195070a7cdaa3c60e9a3597214982da1db17
Lumicran <tienthuyhan@gmail.com>
2019-07-20 17:06:29 -0700

    Clean DB and requirements

--- model.py
+++ model.py
@@ -1,8 +1,7 @@
-"""Model and database functions for Garden Trading App."""
+"""Model and database functions for Affirmation App."""
 from flask_sqlalchemy import SQLAlchemy
 import json
 from datetime import datetime
-# from werkzeug.security import generate_password_hash, check_password_hash
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 
@@ -11,39 +10,20 @@ db = SQLAlchemy()
 # Model definitions
 
 class User(db.Model):
-    """User of garden trading website."""
+    """User of Affirmation website."""
     __tablename__ = "users"
-    __searchable__ = ['username', 'zipcode', 'about_me', 'about_garden']
 
     user_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    fname = db.Column(db.String(55), nullable=False)
+    lname = db.Column(db.String(55), nullable=False)
     username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(255), nullable=False)
     password = db.Column(db.Binary(128), nullable=False)
-    fname = db.Column(db.String(55), nullable=False)
-    lname = db.Column(db.String(55), nullable=False)
     phone = db.Column(db.String(15), nullable=False)
 
-
-
-    #Create relationships for messaging.
-
-    messages_sent = db.relationship('Message',
-                                    foreign_keys='Message.sender_id',
-                                    backref='sender', lazy='dynamic')
-    messages_received = db.relationship('Message',
-                                        foreign_keys='Message.recipient_id',
-                                        backref='recipient', lazy='dynamic')
-    last_message_read_time = db.Column(db.DateTime)
-
-    def new_messages(self):
-        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
-        return Message.query.filter_by(recipient=self).filter(
-            Message.timestamp > last_read_time).count()
-
-    """def set_password(self, password):
-    	self.password_hash = generate_password_hash(password)
-    #def check_password(self, password):
-    	return check_password_hash(self.password_hash, password)"""
+    user_message = db.relationship('Message',
+                                    foreign_keys='Message.user_id',
+                                    backref='user', lazy='dynamic')
 
 def __repr__(self):
     """Provide helpful representation when printed."""
@@ -55,12 +35,11 @@ class Message(db.Model):
     __tablename__= "messages"
 
     message_id = db.Column(db.Integer, primary_key=True)
-    sender_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    recipient_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
-    sender_username = db.Column(db.String(120), nullable = False)
-    recipient_username = db.Column(db.String(120), nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.user_id'))
     body = db.Column(db.String(200), nullable = False)
-    send_date_time = db.Column(db.DateTime, nullable = False, default=datetime.utcnow)
+    scheduled_date = db.Column(db.String(10), nullable = False)
+    scheduled_time = db.Column(db.String(20), nullable = False)
+    sent = db.Column(db.Boolean, nullable=False, default=False)
 
 def __repr__(self):
         return '<Message {}>'.format(self.body)
@@ -69,7 +48,7 @@ def __repr__(self):
 def connect_to_db(app, db_uri= 'postgres:///peptalk'):
     """Connect the database to our Flask app."""
     # Configure to use our database.
-    
+
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     app.config['SQLALCHEMY_ECHO'] = True
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
@@ -80,17 +59,18 @@ def connect_to_db(app, db_uri= 'postgres:///peptalk'):
 
 def example_data(bcrypt):
     """Create example data for the test database."""
-    password = bcrypt.generate_password_hash("gardenlover")
-    user1 = User(username="gardenlover", email="gardenlover@gmail.com",
-                 fname="Jane", lname="Garden", address="3120 De La Cruz Blvd", 
-                 city="Santa Clara", state="CA", zipcode=95054, 
-                 password=password) 
+    user1 = User(fname="Positive",
+                lname="Friend",
+                username="positiveFriend",
+                email="positivity@gmail.com",
+                phone="555-555-5555")
     db.session.add(user1)
-    password2 = bcrypt.generate_password_hash("herbie")
-    user2= User(username="herbie", email="herbie@gmail.com", 
-                
-                fname="Herb", lname="Garden", address="3120 De la Cruz Blvd", city="Santa Clara", state="CA", zipcode=95054, 
-                password=password2)
+
+    user2 = User(fname="Negative",
+                lname="Friend",
+                username="negativeFriend",
+                email="negativity@gmail.com",
+                phone="555-555-5555")
     db.session.add(user2)
 
     db.session.commit()
